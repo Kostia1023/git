bb2d318097db2e17c77bccd7304ca54a2ea398f9
Kostia1023 <74052496+Kostia1023@users.noreply.github.com>
2021-10-06 18:18:06 +0300

    .

---
+++ labs/c#/2 course/Lab3/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    class Person
+    {
+        public string Name;
+        public string Gender;
+        public int YearBirth;
+        public string Schooling;
+        public DateTime DateAppcent;
+
+        public Person() { }
+    }
+}
